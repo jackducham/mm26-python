984ed7367c8b23598a3b162c9525b5d8b5a1b481
allenc3 <allenc3@illinois.edu>
2020-09-19 16:10:35 -0500

    remove unecessary files

--- script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export REDIS_HOST=127.0.0.1
-export REDIS_PORT=6379
-export TEAM_NAME=allen
-export REDIS_PASSWORD=mymothersmaidenname
--- shutdown_gameserver.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-requests.post("http://127.0.0.1:10000/shutdown")
